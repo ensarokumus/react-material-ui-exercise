26d0ee44958edd5611ba1857f74ff4e1c3f3ad41
ensaro <41113027+ensarokumus@users.noreply.github.com>
2023-12-29 21:47:58 +1100

    Use data.json to fill the rest of the cards

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,34 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
-import Grid from '@mui/material/Unstable_Grid2';
+import Grid from "@mui/material/Unstable_Grid2";
 import SearchAppBar from "./components/AppBar";
-
+import cities from "./data.json";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+        {cities.map((city) => (
+          <>
+            <Typography
+              key={city.id}
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour) => (
+                <TourCard key={tour.id} tour={tour} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   );
--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
@@ -5,6 +6,7 @@ import Box from "@mui/material/Box";
 import { AccessTime } from "@mui/icons-material";
 import Rating from "@mui/material/Rating";
 import { createTheme, ThemeProvider } from "@mui/material";
+
 
 const theme = createTheme({
   components: {
@@ -31,19 +33,15 @@ const theme = createTheme({
   },
 });
 
-export default function TourCard() {
+export default function TourCard({ tour }) {
   return (
     <Grid item xs={3}>
       <ThemeProvider theme={theme}>
         <Paper elevation={3}>
-          <img
-            src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg"
-            alt="Niagara falls photo"
-            className="img"
-          />
+          <img src={tour.image} alt="Tour photo" className="img" />
           <Box paddingX={1}>
             <Typography variant="subtitle1" component="h2">
-              Immerse into the Falls
+              {tour.name}
             </Typography>
             <Box
               sx={{
@@ -53,7 +51,7 @@ export default function TourCard() {
             >
               <AccessTime sx={{ width: 12.5 }} />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                5 hours
+                {tour.duration} hours
               </Typography>
             </Box>
             <Box
@@ -65,21 +63,21 @@ export default function TourCard() {
             >
               <Rating
                 name="read-only"
-                value={4.5}
+                value={tour.rating}
                 precision={0.5}
                 size="small"
                 readOnly
               />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                4.5
+                {tour.rating}
               </Typography>
               <Typography variant="body3" component="p" marginLeft={1.5}>
-                (655 reviews)
+                {tour.numberOfReviews} reviews
               </Typography>
             </Box>
             <Box>
               <Typography variant="h6" component="h3" marginTop={0}>
-                From AUD $147
+                From AUD ${tour.price}
               </Typography>
             </Box>
           </Box>
