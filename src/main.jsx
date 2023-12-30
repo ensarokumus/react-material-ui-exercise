9cd3502142ed92909c3f1e3c801b2ca3ddc4ec37
ensaro <41113027+ensarokumus@users.noreply.github.com>
2023-12-31 10:52:03 +1100

    Add React Route to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SearchAppBar from "./components/AppBar";
+import "./index.css";
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Home from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <SearchAppBar />    
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import TourCard from "../components/TourCard";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Unstable_Grid2";
+import cities from "../data.json";
+import { Typography } from "@mui/material";
+
+const Home = () => (
+  <div className="App">
+    <Container sx={{ marginY: 5 }}>
+      {cities.map((city) => (
+        <>
+          <Typography
+            key={city.id}
+            variant="h4"
+            component="h2"
+            marginTop={5}
+            marginBottom={3}
+          >
+            Top {city.name} Tours
+          </Typography>
+          <Grid container spacing={5}>
+            {city.tours.map((tour) => (
+              <TourCard key={tour.id} tour={tour} />
+            ))}
+          </Grid>
+        </>
+      ))}
+    </Container>
+  </div>
+);
+
+export default Home;
