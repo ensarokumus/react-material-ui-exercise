a74137b38cbcf00f113bf4747bea0204a561e917
ensaro <41113027+ensarokumus@users.noreply.github.com>
2023-12-31 14:05:20 +1100

    Add router link to TourCard components

--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -6,6 +6,7 @@ import Box from "@mui/material/Box";
 import { AccessTime } from "@mui/icons-material";
 import Rating from "@mui/material/Rating";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const theme = createTheme({
   components: {
@@ -34,6 +35,7 @@ const theme = createTheme({
 
 TourCard.propTypes = {
   tour: PropTypes.shape({
+    id: PropTypes.number,
     image: PropTypes.string,
     name: PropTypes.string,
     duration: PropTypes.number,
@@ -46,53 +48,55 @@ TourCard.propTypes = {
 export default function TourCard({ tour }) {
   return (
     <Grid item xs={3}>
-      <ThemeProvider theme={theme}>
-        <Paper elevation={3}>
-          <img src={tour.image} alt="Tour photo" className="img" />
-          <Box paddingX={1}>
-            <Typography variant="subtitle1" component="h2">
-              {tour.name}
-            </Typography>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <AccessTime sx={{ width: 12.5 }} />
-              <Typography variant="body2" component="p" marginLeft={0.5}>
-                {tour.duration} hours
+      <Link to={tour.id} style={{ textDecoration: "none" }}>
+        <ThemeProvider theme={theme}>
+          <Paper elevation={3}>
+            <img src={tour.image} alt="Tour photo" className="img" />
+            <Box paddingX={1}>
+              <Typography variant="subtitle1" component="h2">
+                {tour.name}
               </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <AccessTime sx={{ width: 12.5 }} />
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                  {tour.duration} hours
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+                marginTop={3}
+              >
+                <Rating
+                  name="read-only"
+                  value={tour.rating}
+                  precision={0.5}
+                  size="small"
+                  readOnly
+                />
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                  {tour.rating}
+                </Typography>
+                <Typography variant="body3" component="p" marginLeft={1.5}>
+                  {tour.numberOfReviews} reviews
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="h3" marginTop={0}>
+                  From AUD ${tour.price}
+                </Typography>
+              </Box>
             </Box>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-              }}
-              marginTop={3}
-            >
-              <Rating
-                name="read-only"
-                value={tour.rating}
-                precision={0.5}
-                size="small"
-                readOnly
-              />
-              <Typography variant="body2" component="p" marginLeft={0.5}>
-                {tour.rating}
-              </Typography>
-              <Typography variant="body3" component="p" marginLeft={1.5}>
-                {tour.numberOfReviews} reviews
-              </Typography>
-            </Box>
-            <Box>
-              <Typography variant="h6" component="h3" marginTop={0}>
-                From AUD ${tour.price}
-              </Typography>
-            </Box>
-          </Box>
-        </Paper>
-      </ThemeProvider>
+          </Paper>
+        </ThemeProvider>
+      </Link>
     </Grid>
   );
 }
